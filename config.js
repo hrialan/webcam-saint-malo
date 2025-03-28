48839b920d93cc538e093cc4adcbaefb563b0e92
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2025-03-28 15:41:01 +0100

    V2

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+const videoConfig = {
+    videos: [
+        '0l2LxTnQGZc',  // Lien 1
+        'p6nAlz4_bdI',  // Lien 2
+        'fY9NbB8yqFA',  // Lien 3
+        'J3xg6-S2bFU'   // Lien 4
+    ]
+};
