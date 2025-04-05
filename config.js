eda3bdda1ba2908046c4c4899d97ef7eea65a597
Hugo Rialan <hugo.rialan@gmail.com>
2025-04-05 09:44:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const videoConfig = {
         '0l2LxTnQGZc',  // Lien 1
         'p6nAlz4_bdI',  // Lien 2
         'fY9NbB8yqFA',  // Lien 3
-        '4In_qA6dLcg'   // Lien 4
+        'OzYp4NRZlwQ'   // Lien 4
     ]
 };
