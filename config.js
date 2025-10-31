06d86a17867cd9489ab68a172ce8df87c9b800f6
Hugo Rialan <hugo.rialan@gmail.com>
2025-10-31 16:13:30 +0100

    Update video ID in videoConfig

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const videoConfig = {
     videos: [
-        '0l2LxTnQGZc',  // Lien 1
+        '80s06q41pMo',  // Lien 1
         'p6nAlz4_bdI',  // Lien 2
         'fY9NbB8yqFA',  // Lien 3
         'OzYp4NRZlwQ'   // Lien 4
